1f35d0808535eb28e1790f10a4560365396e5ce1
Pierre Bernard <pierre.bernard@etu.univ-grenoble-alpes.fr>
2024-01-24 21:16:44 +0100

    ajout des fichier .java nécessaire au parser

--- JavaProject/src/info3/parser/javacc/projet/langageC/_code/Args.java
+++ JavaProject/src/info3/parser/javacc/projet/langageC/_code/Args.java
@@ -1,6 +1,6 @@
 package info3.parser.javacc.projet.langageC._code;
 
-public class Expr extends Tree implements ITree {
+public class Args extends Tree implements ITree {
 	//TODO classe créée recemment pour le parser.jj
 	
 	@Override
--- JavaProject/src/info3/parser/javacc/projet/langageC/_code/Function.java
+++ JavaProject/src/info3/parser/javacc/projet/langageC/_code/Function.java
@@ -1,8 +1,8 @@
 package info3.parser.javacc.projet.langageC._code;
 
-public class Expr extends Tree implements ITree {
+public class Function extends Tree implements ITree {
 	//TODO classe créée recemment pour le parser.jj
-	
+
 	@Override
 	public String pretty(int d) {
 		// TODO à modifier
