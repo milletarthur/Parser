c36595c52755d6b7056116b5e87d67b9554a3e82
Remi Gaulmin <remi.gaulmin@etu.univ-grenoble-alpes.fr>
2024-01-25 17:43:02 +0100

    Modifs de Pierre et moi qu'il n'avait pas push

--- JavaProject/src/info3/parser/javacc/projet/langageC/_code/Value.java
+++ JavaProject/src/info3/parser/javacc/projet/langageC/_code/Value.java
@@ -3,20 +3,15 @@ package info3.parser.javacc.projet.langageC._code;
 public class Value extends Tree implements ITree {
 
 	String ident;
-	Tree t2;
 
-	public Value(String ident,Tree td) {
-		this.ident = ident;
-		this.t2=td;
+	public Value(Token ident) {
+		this.ident = ident.toString();
 	}
 
 	// PRETTY
 
 	@Override
 	public String pretty(int d) {
-		String s = new String();
-		s+=ident;
-		s+=t2.pretty(d+1);
-		return s;
+		return new String(ident);
 	}
 }
