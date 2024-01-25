49e932726dbb328f5c1d55b1ddecb94f9a84c042
Samuel Lepin <samlepin25@gmail.com>
2024-01-25 13:57:31 +0100

    mis à jour de la class PrePostOp

--- JavaProject/src/info3/parser/javacc/projet/langageC/_code/PrePostOp.java
+++ JavaProject/src/info3/parser/javacc/projet/langageC/_code/PrePostOp.java
@@ -3,14 +3,28 @@ package info3.parser.javacc.projet.langageC._code;
 public class PrePostOp extends Tree implements ITree {
 
 	String prepostop;
+	Tree t;
 	
 	public PrePostOp(String s) {
 		this.prepostop = s;
+		this.t = null;
+	}
+	
+	public PrePostOp(Tree tr) {
+		this.prepostop = null;
+		this.t = tr;
 	}
 	
 	public String pretty(int d) {
 		String string = new String();
-		string += prepostop;
+		if(this.t == null) {
+			string += prepostop;
+		}else {
+			string += Pretty.string(format, "[");
+			string += t.pretty(d);
+			string += Pretty.string(format, "]");
+		}
+		
 		return string;
 	}
 }
