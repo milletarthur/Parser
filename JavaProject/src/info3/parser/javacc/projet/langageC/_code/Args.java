dc35266faac7835abdab554f91bcd4c9a2959c0f
MidnightooP <antoniaivanova306@gmail.com>
2024-01-25 13:33:29 +0100

    J'ai fini Args.java et Expr.java

--- JavaProject/src/info3/parser/javacc/projet/langageC/_code/Args.java
+++ JavaProject/src/info3/parser/javacc/projet/langageC/_code/Args.java
@@ -1,13 +1,23 @@
 package info3.parser.javacc.projet.langageC._code;
 
 public class Args extends Tree implements ITree {
-	//TODO classe créée recemment pour le parser.jj
+	
+	Tree fg,fd;
+	
+	public Args (Tree t1, Tree t2) {
+		fg = t1;
+		fd = t2;
+	}
 	
 	@Override
 	public String pretty(int d) {
-		// TODO à modifier
-		{
-			return null;
+		Expr fg_expr = (Expr) fg;
+		String rv = new String();
+		rv = fg_expr.pretty();
+		if(fd != null) {
+			rv += ",";
+			rv += fd.pretty();
 		}
+		return rv;
 	}
 }
